97c6bfc15a1be3b9ac4b3a2b3cfcea98e2def261
cirodil <cirodil@gmail.com>
2020-06-18 23:22:15 +0300

    add logout message toast

--- src/main.js
+++ src/main.js
@@ -5,12 +5,15 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import dateFilter from '@/filters/date.filter.js'
+import messagePlugin from '@/utils/message.plugin.js'
 import 'materialize-css/dist/js/materialize.min'
 
 Vue.config.productionTip = false
 
+Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
+
 
 new Vue({
   router,
