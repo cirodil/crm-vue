5739e76eee8dfbf25a7c94c611b6cb6a19beb2e5
cirodil <cirodil@gmail.com>
2020-07-15 22:49:19 +0300

    add bill info

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -15,7 +15,7 @@
             data-target="dropdown"
             ref="dropdown"
           >
-            USER NAME
+            {{name}}
             <i class="material-icons right">arrow_drop_down</i>
           </a>
 
@@ -49,6 +49,11 @@ export default {
     async logout() {
      await this.$store.dispatch('logout')
       this.$router.push('/login?message=logout')
+    }
+  },
+  computed: {
+    name() {
+      return this.$store.getters.info.name
     }
   },
   mounted() {
