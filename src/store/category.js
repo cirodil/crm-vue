2cd81174cb43eb0e960d9c77dae87ee077922bcb
cirodil <cirodil@gmail.com>
2020-08-14 22:41:55 +0300

    add 'fetchCategories' action

--- src/store/category.js
+++ src/store/category.js
@@ -2,6 +2,16 @@ import firebase from 'firebase/app'
 
 export default {
   actions: {
+    async fetchCategories({commit, dispatch}) {
+      try {
+        const uid = await dispatch('getUid')
+        const categories = (await firebase.database().ref(`/users/${uid}/categories`).once('value')).val() || {}
+        return Object.keys(categories).map(key => ({...categories[key], id: key})) 
+      } catch (e) {
+        commit('setError', e)
+        throw e
+      }
+    },
     async createCategory({commit, dispatch}, {title, limit}) {
       try {
         const uid = await dispatch('getUid')
@@ -14,3 +24,4 @@ export default {
     }
   }
 }
+// https://youtu.be/wCuv_s77A1U?t=561
